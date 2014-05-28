df9e2c05c32d1e3d6a5a386dcb60e56bd35389cb
Russell Fair <rfair404@gmail.com>
2014-05-28 12:15:14 -0400

    go the new route on updates

---
+++ class-network-sidebar-fallback.php
@@ -0,0 +1,109 @@
+<?php
+
+class Network_Sidebar_Fallback{
+    public $sidebars, $cache_time;
+
+    function __construct(){
+        add_action('init', array($this, 'set_sidebars'), 10);
+        add_action('init', array($this, 'set_cache_time'), 10);
+
+        add_action('dynamic_sidebar_before', array($this, 'network_sidebar'), 10, 2);
+        add_action( 'init', array($this, 'collect_sidebars'), 10);
+        add_action( 'init', array($this, 'refresh_sidebars'), 10);
+        add_filter('widget_update_callback', array($this, 'widget_change'), 10, 4);
+    }
+
+    /**
+    * gets the registered sidebars and sets $this->sidebars
+    */
+    function set_sidebars(){
+        global $wp_registered_sidebars;
+        $this->sidebars = $wp_registered_sidebars;
+    }
+
+
+    /**
+    * gets the registered sidebars and sets $this->sidebars
+    * provides access to shorten or lengthen the cache time
+    */
+    function set_cache_timeout(){
+        $this->cache_time = apply_filters('nsf_cache_time', 60*60);
+    }
+
+    /**
+    * Displays the network wide sidebar when existing widgets aren't there
+    */
+    function network_sidebar($index, $has_widgets){
+        if($has_widgets || is_admin() || is_main_site() ){
+            //this only does something when the child site sidebar is empty
+            return;
+        }
+        echo get_site_transient( 'nsf_' . $index . '_widgets' );
+    }
+
+    /**
+    * caches the sidebars if the transient doesn't exist (reload)
+    */
+    function collect_sidebars(){
+        if ( ! is_main_site() ){
+            //prevent non-child sites from messing this up
+            return;
+        }
+
+        foreach ($this->sidebars as $sidebar => $sidebar_args){
+            if(!($this_sidebar = get_site_transient( 'nsf_' . $sidebar  . '_widgets' ))){
+                $this->cache_sidebar($sidebar);
+            }
+        }
+    }
+
+
+    /**
+    * refreshes all sidebars
+    */
+    function refresh_sidebars(){
+        if ( ! is_main_site() ){
+            return;
+        }
+
+        if ( get_site_option('nsf_refresh_required') != true ){
+            return;
+        }
+
+        foreach ($this->sidebars as $sidebar => $sidebar_args){
+            $this->cache_sidebar($sidebar);
+        }
+    }
+
+    /**
+    * caches a single sidebar
+    * @param string $sidebar_id the id (slug) of the sidebar to update
+    */
+    function cache_sidebar($sidebar_id){
+        ob_start();
+        dynamic_sidebar( $sidebar_id );
+
+        $sidebar_output = ob_get_contents();
+        ob_end_clean();
+        //saves for one hour
+        set_site_transient( 'nsf_' . $sidebar_id . '_widgets', $sidebar_output, $this->cache_time );
+    }
+
+    /**
+    * attaches to the widget save option and triggers the cache refresh
+    * @return array $new the new widget instance
+    */
+    function widget_change($instance, $new, $old, $this_widget){
+        if ( is_main_site() ){
+            $this->trigger_refresh();
+        }
+        return $new;
+    }
+
+    /**
+    * forces wp to do a sidebar cache reload on next init
+    */
+    function trigger_refresh(){
+        update_site_option('nsf_refresh_required', true);
+    }
+}
--- network-sidebar-fallback.php
+++ network-sidebar-fallback.php
@@ -1,75 +1,12 @@
 <?php
 /*
-* Plugin Name: Network Sidebar Fallback
+* Plugin Name: Network Sidebar Widget Fallback
 * Author: Russell Fair
-* Version: 0.2
-* Description: Uses the parent site's sidebar as the fallback for non-widgetized sidebars
+* Version: 0.3
+* Description: Provides an intuative way of using the same sidebar accross a network of sites. Basically makes the "parent site" the fallback in situations where the "child site" doesn't have a sidebar configured.
 */
 
-function nsf_network_sidebar($index, $has_widgets){
-    if($has_widgets || is_admin()){
-        //this only does something when the sidebar is empty
-        return;
-    }
-    echo get_site_transient( 'nsf_' . $index . '_widgets' );
-}
-add_action('dynamic_sidebar_before', 'nsf_network_sidebar', 10, 2);
-
-function nsf_gather_sidebar_output(){
-
-    if ( ! is_main_site() ){
-        //prevent non-child sites from messing this up
-        return;
-    }
-
-    global $wp_registered_sidebars;
-    foreach ($wp_registered_sidebars as $wp_registered_sidebar => $sidebar_args){
-        if(!($this_sidebar = get_site_transient( 'nsf_' . $wp_registered_sidebar  . '_widgets' ))){
-            cache_sidebar($wp_registered_sidebar);
-        }
-    }
-}
-add_action( 'init', 'nsf_gather_sidebar_output');
-
-function cache_sidebar($sidebar_id){
-    ob_start();
-    if ( ! dynamic_sidebar( $sidebar_id ) ) :
-        endif; // end sidebar widget area
-
-    $sidebar_output = ob_get_contents();
-    ob_end_clean();
-    //saves for one hour
-    set_site_transient( 'nsf_' . $sidebar_id . '_widgets', $sidebar_output, 60*60 );
-}
-
-/**
- * Enqueue and localize our scripts
- */
-add_action( 'admin_enqueue_scripts', 'nsf_enqueue_ajax_scripts' );
-function nsf_enqueue_ajax_scripts() {
-    global $current_screen;
-    $version = (int) '2';
-
-    // Only register these scripts if we're on the widgets page
-    if ( $current_screen->id == 'widgets' ) {
-        wp_enqueue_script( 'nsf_ajax_scripts', plugins_url( '/js/widget-actions.js' , __FILE__ ), array( 'jquery' ), $version, true );
-        wp_localize_script( 'nsf_ajax_scripts', 'nsf_AJAX', array( 'nsf_widget_nonce' => wp_create_nonce( 'widget-update-nonce' ) ) );
-    }
-}
-/**
- * Register our AJAX call
- */
-add_action( 'wp_ajax_nsf-reset-transient', 'nsf_clear_transient', 1 );
-
-/**
- * AJAX Helper to delete our transient when a widget is saved
- */
-function nsf_clear_transient() {
-
-    // Delete our footer transient.  This runs when a widget is saved or updated.  Only do this if our nonce is passed.
-    if ( ! empty( $_REQUEST['nsf-widget-nonce'] ) ){
-        delete_site_transient( 'nsf_'.$_REQUEST['sidebar'].'_widgets' );
-        cache_sidebar($_REQUEST['sidebar']);
-    }
-    die();
+require_once('class-network-sidebar-fallback.php');
+if(class_exists('Network_Sidebar_Fallback')){
+    new Network_Sidebar_Fallback;
 }
