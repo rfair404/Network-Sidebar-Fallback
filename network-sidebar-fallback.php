e7d544d3ecc2ad37a3da18e8d488eaa31f3e6b0c
Russell Fair <rfair404@gmail.com>
2016-07-28 18:53:57 +0000

    update version # to 0.1

--- network-sidebar-fallback.php
+++ network-sidebar-fallback.php
@@ -2,7 +2,7 @@
 /*
 * Plugin Name: Network Sidebar Widget Fallback
 * Author: Russell Fair
-* Version: 0.3
+* Version: 0.1
 * Description: Provides an intuative way of using the same sidebar accross a network of sites. Basically makes the "parent site" the fallback in situations where the "child site" doesn't have a sidebar configured.
 */
 
